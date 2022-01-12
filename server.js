198c63f7b9b359f2e9412e7795784e7898cab7ec
Shaun S <shaun.sticka@outlook.com>
2022-01-11 20:59:02 -0600

    added much stuff for server and for user model and connectivity

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ const sequelize = require("./config/connection");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
